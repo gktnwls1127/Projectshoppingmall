0df1713469f3ff78c501d95203d6acaee46ef232
gktnwls1127 <gktnwls1127@naver.com>
2020-07-20 13:50:34 +0900

    th

--- client/src/components/views/Shoppingmall/LandingPage/Sections/HotCards.js
+++ client/src/components/views/Shoppingmall/LandingPage/Sections/HotCards.js
@@ -139,7 +139,7 @@ function HotCards() {
                 
                         <div className="more_button">
                                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                    <a className="more_a" href="/shopppingmall/best" >아우터 더보기
+                                    <a className="more_a" href="/shopppingmall/outer" >아우터 더보기
                                         <svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" class="sc-Axmtr StoreSectionLinkButtonIcon___StyledCaretRight-gkYQiY iMnOGp">
                                             <path fill-rule="evenodd" d="M9.5 18.66c.2.002.39-.078.53-.22l6-6a.75.75 0 000-1.06l-6-6A.75.75 0 009 6.44l5.44 5.47L9 17.38a.75.75 0 00.5 1.28z"></path>
                                         </svg>
@@ -160,7 +160,7 @@ function HotCards() {
                 
                         <div className="more_button">
                                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                    <a className="more_a" href="/shopppingmall/best" >상의 더보기
+                                    <a className="more_a" href="/shopppingmall/shirt_blouse" >상의 더보기
                                         <svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" class="sc-Axmtr StoreSectionLinkButtonIcon___StyledCaretRight-gkYQiY iMnOGp">
                                             <path fill-rule="evenodd" d="M9.5 18.66c.2.002.39-.078.53-.22l6-6a.75.75 0 000-1.06l-6-6A.75.75 0 009 6.44l5.44 5.47L9 17.38a.75.75 0 00.5 1.28z"></path>
                                         </svg>
@@ -181,7 +181,7 @@ function HotCards() {
                 
                         <div className="more_button">
                                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                    <a className="more_a" href="/shopppingmall/best" >바지 더보기
+                                    <a className="more_a" href="/shopppingmall/pants" >바지 더보기
                                         <svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" class="sc-Axmtr StoreSectionLinkButtonIcon___StyledCaretRight-gkYQiY iMnOGp">
                                             <path fill-rule="evenodd" d="M9.5 18.66c.2.002.39-.078.53-.22l6-6a.75.75 0 000-1.06l-6-6A.75.75 0 009 6.44l5.44 5.47L9 17.38a.75.75 0 00.5 1.28z"></path>
                                         </svg>
@@ -202,7 +202,7 @@ function HotCards() {
                 
                         <div className="more_button">
                                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                    <a className="more_a" href="/shopppingmall/best" >원피스 더보기
+                                    <a className="more_a" href="/shopppingmall/onepiece" >원피스 더보기
                                         <svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" class="sc-Axmtr StoreSectionLinkButtonIcon___StyledCaretRight-gkYQiY iMnOGp">
                                             <path fill-rule="evenodd" d="M9.5 18.66c.2.002.39-.078.53-.22l6-6a.75.75 0 000-1.06l-6-6A.75.75 0 009 6.44l5.44 5.47L9 17.38a.75.75 0 00.5 1.28z"></path>
                                         </svg>
@@ -223,7 +223,7 @@ function HotCards() {
                 
                         <div className="more_button">
                                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                    <a className="more_a" href="/shopppingmall/best" >치마 더보기
+                                    <a className="more_a" href="/shopppingmall/skirt" >치마 더보기
                                         <svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" class="sc-Axmtr StoreSectionLinkButtonIcon___StyledCaretRight-gkYQiY iMnOGp">
                                             <path fill-rule="evenodd" d="M9.5 18.66c.2.002.39-.078.53-.22l6-6a.75.75 0 000-1.06l-6-6A.75.75 0 009 6.44l5.44 5.47L9 17.38a.75.75 0 00.5 1.28z"></path>
                                         </svg>
@@ -244,7 +244,7 @@ function HotCards() {
                 
                         <div className="more_button">
                                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                    <a className="more_a" href="/shopppingmall/best" >신발 더보기
+                                    <a className="more_a" href="/shopppingmall/shoes" >신발 더보기
                                         <svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" class="sc-Axmtr StoreSectionLinkButtonIcon___StyledCaretRight-gkYQiY iMnOGp">
                                             <path fill-rule="evenodd" d="M9.5 18.66c.2.002.39-.078.53-.22l6-6a.75.75 0 000-1.06l-6-6A.75.75 0 009 6.44l5.44 5.47L9 17.38a.75.75 0 00.5 1.28z"></path>
                                         </svg>
