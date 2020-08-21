e2644de42a788da26866825ca483e836a79a797f
jay <jay0764@naver.com>
2020-08-21 16:12:09 +0900

    dislike 부분 삭제

--- server/routes/like.js
+++ server/routes/like.js
@@ -20,19 +20,6 @@ router.post('/getLikes', (req, res) => {
 	});
 });
 
-router.post('/getDisLikes', (req, res) => {
-	let variable = {};
-
-	if (req.body.commentId) {
-		variable = { commentId: req.body.commentId };
-	}
-
-	Dislike.find(variable).exec((err, dislikes) => {
-		if (err) return res.status(400).send(err);
-		res.status(200).json({ success: true, dislikes });
-	});
-});
-
 router.post('/upLike', (req, res) => {
 	let variable = {};
 
@@ -63,38 +50,6 @@ router.post('/unLike', (req, res) => {
 	Like.findOneAndDelete(variable).exec((err, result) => {
 		if (err) return res.status(400).json({ success: false, err });
 		res.status(200).json({ success: true });
-	});
-});
-
-router.post('/unDislike', (req, res) => {
-	let variable = {};
-
-	if (req.body.commentId) {
-		ariable = { commentId: req.body.commentId, userId: req.body.userId };
-	}
-
-	Dislike.findOneAndDelete(variable).exec((err, result) => {
-		if (err) return res.status(400).json({ success: false, err });
-		res.status(200).json({ success: true });
-	});
-});
-
-router.post('/upDislike', (req, res) => {
-	let variable = {};
-
-	if (req.body.commentId) {
-		ariable = { commentId: req.body.commentId, userId: req.body.userId };
-	}
-
-	const dislike = new Dislike(variable);
-
-	dislike.save((err, dislikeResult) => {
-		if (err) return res.status(400).json({ success: false, err });
-		//만약 Dislike 이 이미 클릭 되어 있다면 , dislike을 1 줄여준다.
-		Like.findOneAndDelete(variable).exec((err, likeResult) => {
-			if (err) return res.status(400).json({ success: false, err });
-			res.status(200).json({ success: true });
-		});
 	});
 });
 
