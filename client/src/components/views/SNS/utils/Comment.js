76262ccb3912ad6180f99b9efa960f3e8603827d
jay <jay0764@naver.com>
2020-08-24 13:33:01 +0900

    랜딩페이지 댓글 로직 수정 - count만 보이게

--- client/src/components/views/SNS/utils/Comment.js
+++ client/src/components/views/SNS/utils/Comment.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import sanitize from 'sanitize-html';
+
 function Comment(props) {
 	const [comments, setComments] = useState([]);
 
@@ -13,55 +13,8 @@ function Comment(props) {
 			}
 		});
 	}, []);
-	const filterComment = (text) => {
-		let preText = text.split(' ');
-		let postingText = '';
 
-		preText.forEach((content) => {
-			if (typeof content !== 'undefined') {
-				if (content.charAt(0) === '#') {
-					postingText += `<a href="/search/${content.substr(
-						1
-					)}">${content} </a>`;
-				} else {
-					postingText += content + ' ';
-				}
-			}
-		});
-		return { __html: sanitize(postingText) };
-	};
-	const renderComments = () => {
-		if (comments.length >= 2) {
-			let slicedComments = comments.slice(0, 2);
-			return slicedComments.map((comment) => (
-				<div key={comment._id}>
-					<img
-						style={{ width: '50px', height: '50px' }}
-						src={`http://localhost:5000/${comment.writer.image}`}
-						alt="유저이미지"
-					/>
-					<h3>{comment.writer.name}</h3>
-					<p dangerouslySetInnerHTML={filterComment(comment.comment)}></p>
-				</div>
-			));
-		} else if (comments.length == 1) {
-			return comments.map((comment) => (
-				<div key={comment._id}>
-					<img
-						style={{ width: '50px', height: '50px' }}
-						src={`http://localhost:5000/${comment.writer.image}`}
-						alt="유저이미지"
-					/>
-					<h3>{comment.writer.name}</h3>
-					<p dangerouslySetInnerHTML={filterComment(comment.comment)}></p>
-				</div>
-			));
-		} else {
-			return;
-		}
-	};
-
-	return <div>{renderComments()}</div>;
+	return <div>{comments.length}</div>;
 }
 
 export default Comment;
